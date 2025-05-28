3501b9f8df3c773ad863ca6cad5a27b708b0dba8
Adam <adam.kern@duke.edu>
2025-05-28 15:41:54 -0400

    Rename

--- Game.lean
+++ Game.lean
@@ -1,7 +1,7 @@
 import Game.Levels.DemoWorld
 
 -- Here's what we'll put on the title screen
-Title "Hello World Game"
+Title "Linear Algebra Game"
 Introduction
 "
 This text appears on the starting page where one selects the world/level to play.
