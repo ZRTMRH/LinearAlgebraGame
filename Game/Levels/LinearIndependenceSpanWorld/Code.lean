ecf235c59d527b2df83f4203d534135536c82803
justin-t-morrill <jtmorrill304@gmail.com>
2025-06-16 16:17:05 -0400

    Simplifying proofs

--- Game/Levels/LinearIndependenceSpanWorld/Code.lean
+++ Game/Levels/LinearIndependenceSpanWorld/Code.lean
@@ -30,7 +30,7 @@ theorem mem_span_of_mem {S : Set V} {v : V} (hv : v ∈ S) : v ∈ span K V S :=
 
 /-- **Level 8: Monotonicity of Span**
 If $A \subseteq B$ are sets of vectors, then `span A ⊆ span B`. In other words, a larger set of vectors can only have a larger (or equal) span. -/
-theorem my_span_mono {K V :Type} [Field K] [AddCommGroup V][VectorSpace K V]{A B : Set V} (hAB : A ⊆ B) : span K V A ⊆ span K V B := by
+theorem span_mono {K V :Type} [Field K] [AddCommGroup V][VectorSpace K V]{A B : Set V} (hAB : A ⊆ B) : span K V A ⊆ span K V B := by
   intro x hxA
   unfold span at *
   unfold is_linear_combination at *
@@ -43,12 +43,35 @@ theorem my_span_mono {K V :Type} [Field K] [AddCommGroup V][VectorSpace K V]{A B
 
 /-- Linear Independence**
 A set of vectors $S$ is **linearly independent** if no vector in $S$ can be written as a linear combination of the others. Equivalently, the only solution to a linear combination of elements of $S$ equaling zero is the trivial solution (all coefficients zero). Here we formalize this condition. -/
-def linear_independent_1 (S : Set V) : Prop :=
+def linear_independent_v (S : Set V) : Prop :=
 ∀ (s : Finset V) (f : V → K),
 (↑s ⊆ S) → (Finset.sum s (fun v ↦ f v • v) = 0) → (∀ v ∈ s, f v = 0)
 
 /-- Independence of Empty Set and Singletons**
 Proposition 2.6(a): The empty set is linearly independent (vacuously, since there are no vectors to depend on). -/
-theorem linear_independent_empty (K V : Type) [Field K] [AddCommGroup V] [VectorSpace K V] : linear_independent_1 K V (∅ : Set V) := by
-  intros s f hs sum_zero v hv
-  exact False.elim (hs hv)
+theorem linear_independent_empty (K V : Type) [Field K] [AddCommGroup V] [VectorSpace K V] : linear_independent_v K V (∅ : Set V) := by
+  unfold linear_independent_v
+  intros _s f hs _sum_zero v hv
+  exfalso
+  exact hs hv
+
+/--  Independence of Subsets**
+**Proposition 2.7(a):** Any subset of a linearly independent set is also linearly independent. -/
+theorem subset_linear_independents [Field K] [AddCommGroup V][VectorSpace K V] {A B : Set V} (hBsubA : B ⊆ A) (hA : linear_independent_v K V A) :
+    linear_independent_v K V B := by
+  unfold linear_independent_v at *
+  intros s f hsB sum_zero v hv
+  have hsA : ↑s ⊆ A := hsB.trans hBsubA
+  exact hA s f hsA sum_zero v hv
+
+
+/-- **Proposition 2.7(b):** If a set $A$ spans the whole space $V$, then any superset of $A$ also spans $V`. Adding more vectors can't reduce the span. -/
+theorem superset_span_full {A B T: Set V} (hT: ∀ (x: V), x ∈ T)(hA : T = span K V A) (hAsubB : A ⊆ B) :
+    T = span K V B := by
+  -- `span A = T` means A is a spanning set for V (the entire space).
+  -- We want to show `span B = V` as well.
+  apply Set.eq_of_subset_of_subset
+  rw [hA]
+  exact span_mono hAsubB
+  intros x _ssg
+  exact hT x
