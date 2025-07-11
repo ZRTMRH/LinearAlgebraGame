26b662ff58f0778cbebe40e38bbeab5ad87a50f3
ZRTMRH <rz169@duke.edu>
2025-07-11 15:36:25 -0400

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -56,3 +56,9 @@ package Game where
 
 @[default_target]
 lean_lib Game
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
